43eff84f225a41e136bec5e61cb598aaf6d380a4
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-07-01 22:01:47 -0700

    Create Routes for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,26 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => res.send("Profile Route"));
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/profile");
+//const User = require("../../models/user");
+
+// route GET api/profile/me
+// desc Get Current user profile
+// access private
+// using token
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no Profile for the User" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error...");
+  }
+});
 module.exports = router;
