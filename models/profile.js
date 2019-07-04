432d5d928470e74f609824bb348a8c4d90f175bb
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-07-04 03:47:45 -0700

    in profile Schema we define experience

--- models/profile.js
+++ models/profile.js
@@ -29,13 +29,31 @@ const profileSchema = new mongoose.Schema({
   },
   experience: [
     {
-      title: {},
-      company: {},
-      location: {},
-      from: {},
-      to: {},
-      current: {},
-      description: {}
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
     }
   ],
   education: [
