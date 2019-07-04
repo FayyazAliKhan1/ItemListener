5e77f7a73bdb5c9dfbefb293326b830fa615560b
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-07-03 22:03:18 -0700

    GET request for getting all profiles and Single Profile also

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -87,7 +87,7 @@ router.post(
           { $set: profileFields },
           { new: true }
         );
-        return res.json(profile);
+        res.json(profile);
       }
       // Create Profile if not
       profile = new Profile(profileFields);
@@ -99,5 +99,34 @@ router.post(
     }
   }
 );
+// route GET api/profile
+// desc GET all profiles
+// access Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+// route GET api/profile/user/:user_id
+// desc GET profile by user id
+// access Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
 
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
