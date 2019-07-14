3fc938efefabadc64f663b849ca81ad86c9075a8
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-07-14 18:01:14 +0500

    Create Component Alert and make store for App

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
-const Register = () => {
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -13,7 +16,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Password do not match");
+      setAlert("Password do not match", "danger");
     } else {
       console.log("SUCCESS");
       // const newUser = {
@@ -97,5 +100,10 @@ const Register = () => {
     </Fragment>
   );
 };
-
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+export default connect(
+  null,
+  { setAlert }
+)(Register);
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initalState = {};
@@ -8,6 +8,6 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initalState,
-  composeWithDevTools(...middleware)
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 export default store;
