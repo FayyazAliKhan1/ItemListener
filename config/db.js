e03a4ec7efc889794a4710dba750ebe93642c40f
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-07-14 11:08:20 +0500

    Make first Simple login and signUp

--- config/db.js
+++ config/db.js
@@ -3,7 +3,11 @@ const config = require("config");
 const db = config.get("mongoURI");
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
     console.log("MongoDB Connected");
   } catch (err) {
     console.log(err.message);
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 connectDB();
 //middleware
 app.use(express.json({ extended: false }));
