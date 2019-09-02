d99fa56be1752145870dfd3636edcefefc82f1a8
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-09-02 10:39:46 +0500

    Add Posts functionalities in this file

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,13 +10,16 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to="/profiles">Developers</Link>
       </li>
       <li>
+        <Link to="/posts">Posts</Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user" />{" "}
           <span className="hide-sm">Dashboard</span>
         </Link>
       </li>
       <li>
-        <a onClick={logout} href="!#">
+        <a onClick={logout} href="/">
           <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
         </a>
